899d8da9735220ed92582e091c84b54903dcf6ae
kshitijkorde <kshi.korde@gmail.com>
2017-09-12 22:13:37 +0100

    Creating a dictionary with lower case & upper case alphabets as keys

---
+++ Dict_Alphabet_Count.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+import string
+dict_alpha_small = dict.fromkeys(string.ascii_lowercase, 0)
+print dict_alpha_small
+dict_alpha_cap   = dict.fromkeys(string.ascii_uppercase, 0)
+print dict_alpha_cap
+
