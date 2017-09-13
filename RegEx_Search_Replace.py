9bdac687d33d9496164d6558d2f80fad8734623c
kshitijkorde <kshi.korde@gmail.com>
2017-09-13 13:05:41 +0100

    Searching for a pattern and replacing it using re.sub()

---
+++ RegEx_Search_Replace.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+# Replace everything inside all the square brackets with _
+import re
+line = "asdfaf[asdfad]asdfadf[23123]asdfa23424[645235$55]asdfaf"	
+print line
+line = re.sub(r'\[\w+\]','_', line) # => asdfaf_asdfadf_asdfa23424[645235$55]asdfaf
+line = re.sub(r'\[\S+\]','_', line) # => asdfaf_asdfadf_asdfa23424_asdfaf
+print line
+
+# Output
+# asdfaf[asdfad]asdfadf[23123]asdfa23424[645235$55]asdfaf
+# asdfaf_asdfadf_asdfa23424_asdfaf
