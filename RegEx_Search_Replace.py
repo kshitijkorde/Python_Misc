98f6c96e3339ff758cff1aebb692895471437b90
kshitijkorde <kshi.korde@gmail.com>
2017-09-14 11:25:13 +0100

    Make a search/replace non greedy

--- RegEx_Search_Replace.py
+++ RegEx_Search_Replace.py
@@ -5,7 +5,11 @@ import re
 line = "asdfaf[asdfad]asdfadf[23123]asdfa23424[645235$55]asdfaf"	
 print line
 line = re.sub(r'\[\w+\]','_', line) # => asdfaf_asdfadf_asdfa23424[645235$55]asdfaf
-line = re.sub(r'\[\S+\]','_', line) # => asdfaf_asdfadf_asdfa23424_asdfaf
+print line
+
+line = "asdfaf[asdfad]asdfadf[23123]asdfa23424[645235$55]asdfaf"	
+print line
+line = re.sub(r'\[\S+?\]','_', line) # => asdfaf_asdfadf_asdfa23424_asdfaf (Note: Adding ? makes it non-greedy)
 print line
 
 # Output
