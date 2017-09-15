8801712369a315cb027ba32f769f887434d00756
kshitijkorde <kshi.korde@gmail.com>
2017-09-15 11:42:35 +0100

    Accepting the command line inputs in python

---
+++ Command_Line_Inputs.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python 
+import sys
+
+print "".join(sys.argv)
+print "Arg 0:"+sys.argv[0]
+print "Arg 1:"+sys.argv[1]
+print "Arg 2:"+sys.argv[2]
+print "Arg 3:"+sys.argv[3]
+print "Arg 4:"+sys.argv[4]
